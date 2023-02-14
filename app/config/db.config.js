dee0a138e68477beeec37d9770ea0ca1660faae9
Timothy Jennike <jenniketimothy00@gmail.com>
2023-02-14 10:44:34 +0200

    origin

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "bc5d4i1hiswchyx9trlj-mysql.services.clever-cloud.com",
+    USER: "uxpwndjvos6jn3mo",
+    PASSWORD: "wQ98SFQIcZuTjStRotWn",
+    DB: "bc5d4i1hiswchyx9trlj",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const db = require("./app/models");
+db.sequelize.sync({force: true}).then(()=>{
+    console.log("Drop and re-sync db");
+});
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res)=> {
+    res.json({message: "This is an attempt to connect."});
+});
+
+require("./app/routes/tutorial.routes.js")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, ()=>{
+    console.log(`Server is running at ${PORT}.`);
+});
+
+
+
+
+
+
